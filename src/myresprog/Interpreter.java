d6cfee45083523e9ab0019fb5a66699e6b7741c2
dennisschmock <dennisschmock@10.50.130.77>
2016-01-14 09:05:14 +0100

    Merge origin/LoopSwichDaniel
    Conflicts:
    	src/myresprog/Interpreter.java

--- src/myresprog/Interpreter.java
+++ src/myresprog/Interpreter.java
@@ -25,13 +25,10 @@ public class Interpreter {
     private int programCounter;
     private MainPanel mainPanel;
     private double angle;
-    private boolean repeat;
     private HashMap vars = new HashMap();
-    private int countRepeat;
-    private int repeats;
-    private int startRepeat;
-    private int endRepeat;
     private ArrayList<Loop> loops;
+    private HashMap procedures = new HashMap();
+    private ArrayList<Procedure> runningProcedures = new ArrayList<>();
 
     public Interpreter(MainPanel panel) {
         mainPanel = panel;
@@ -39,9 +36,10 @@ public class Interpreter {
         commands = lr.textFileToStringArrayList();
         angle = 0;
         oldPoint = new Point(400, 300);
+        currentPoint = oldPoint;
         loops = new ArrayList<>();
+        scanForProcedures();
 
-        //   oldPoint = currentPoint;
     }
 
     public void performNextAction() {
@@ -68,10 +66,10 @@ public class Interpreter {
                 oldPoint = currentPoint;
                 break;
             case "rt":
-                angle = angle - Double.parseDouble(command[1]);
+                angle = angle + Double.parseDouble(command[1]);
                 break;
             case "lt":
-                angle = angle + Double.parseDouble(command[1]);
+                angle = angle - Double.parseDouble(command[1]);
                 break;
             case "let":
                 String expString = "";
@@ -88,8 +86,17 @@ public class Interpreter {
             case "color":
                 changeColor(command);
                 break;
-            case "move":
-                movePointer(command);
+            case "mf":
+                movePointerForward(command);
+                break;
+            case "mb":
+                movePointerBack(command);
+                break;
+            case "declare":
+                skipProcedure(command);
+                break;
+            case "call":
+                callProcedure(command);
                 break;
 
         }
@@ -123,19 +130,132 @@ public class Interpreter {
                 intLoop.setIterations(intLoop.getIterations() - 1);
 
             }
-//            programCounter = startRepeat;
-//            countRepeat++;
 
         }
 
         if (isNumeric(command[1])) {
-            loops.add(new Loop("Loop" + programCounter, programCounter, 0, Integer.parseInt(command[1])-1));
-            startRepeat = programCounter;
-            repeats = Integer.parseInt(command[1]);
-            countRepeat = 0;
+            loops.add(new Loop("Loop" + programCounter, programCounter, 0, Integer.parseInt(command[1]) - 1));
 
         }
 
+    }
+
+    private void scanForProcedures() {
+        int tempInt = 0;
+        int endProc;
+        
+        for (String[] command1 : commands) {
+            if (command1.length<=1){
+                System.out.println("Do nothing");
+            }
+            else if (command1[0].equalsIgnoreCase("declare") && !command1[1].equalsIgnoreCase("end")) {
+                endProc = findEndProc(commands, tempInt);
+                declareProcedure(command1, tempInt, endProc);
+            }
+            tempInt++;
+        }
+
+    }
+
+    private int findEndProc(ArrayList<String[]> commands, int tempInt) {
+        for (int i = tempInt; i < commands.size(); i++) {
+            String tempCommand[] = commands.get(i);
+            if (tempCommand[0].equalsIgnoreCase("declare") && tempCommand[1].equalsIgnoreCase("end")) {
+                return i;
+            }
+
+        }
+        return 0;
+
+    }
+
+    private void skipProcedure(String[] command) {
+        if (command.length <= 1) {
+            return;
+        }
+        if (command[1].equalsIgnoreCase("end")){
+            Procedure tempProc = runningProcedures.get(runningProcedures.size()-1);
+            programCounter = tempProc.getCallPoint();
+            runningProcedures.remove(tempProc);
+            return;
+        }
+        System.out.println(command[1]);
+        Procedure tempProc = (Procedure)procedures.get(command[1]);
+        System.out.println(tempProc);
+        programCounter = tempProc.getProcEnd();
+
+    }
+
+    private void declareProcedure(String[] command, int start, int end) {
+        if (command.length <= 1) {
+            return;
+        }
+        Procedure tempProc = new Procedure(command[1], start, end);
+        procedures.put(command[1], tempProc);
+        if (command.length > 2) {
+            for (int i = 2; i < command.length; i++) {
+                tempProc.getLocalVars().put(command[i], 0);
+
+            }
+
+        }
+
+    }
+
+    private void callProcedure(String[] command) {
+        System.out.println("Trying to call");
+        if(command.length<=1){
+            return;
+        }
+        Procedure tempProc = (Procedure)procedures.get(command[1]);
+        System.out.println(tempProc.getProcName());
+        tempProc.setCallPoint(programCounter);
+        programCounter = tempProc.getProcStart();
+        runningProcedures.add(tempProc);
+        
+
+    }
+
+    private void changeColor(String[] s) {
+        String temp = "";
+        if (s.length > 1) {
+            temp = s[1];
+        }
+        // s.toLowerCase();
+        switch (temp) {
+            case "red":
+                mainPanel.setColor(Color.RED);
+                break;
+            case "yellow":
+                mainPanel.setColor(Color.YELLOW);
+                break;
+            case "green":
+                mainPanel.setColor(Color.GREEN);
+                break;
+            case "blue":
+                mainPanel.setColor(Color.BLUE);
+                break;
+            case "black":
+                mainPanel.setColor(Color.BLACK);
+                break;
+            default:
+                mainPanel.setColor(Color.BLACK);
+                break;
+        }
+    }
+
+    private void movePointerForward(String[] command) {
+        if (isNumeric(command[1])) {
+            currentPoint = FindPoint.findNewPoint(oldPoint, Integer.parseInt(command[1]), angle);
+        }
+        oldPoint = currentPoint;
+    }
+
+    private void movePointerBack(String[] command) {
+        if (isNumeric(command[1])) {
+            currentPoint = FindPoint.findNewPoint(oldPoint, Integer.parseInt(command[1]) * -1, angle);
+        }
+        oldPoint = currentPoint;
     }
 
     /**
@@ -194,36 +314,4 @@ public class Interpreter {
         programCounter = aProgramCounter;
     }
 
-    private void changeColor(String[] s) {
-        String temp = "";
-        if (s.length > 1) {
-            temp = s[1];
-        }
-        // s.toLowerCase();
-        switch (temp) {
-            case "red":
-                mainPanel.setColor(Color.RED);
-                break;
-            case "yellow":
-                mainPanel.setColor(Color.YELLOW);
-                break;
-            case "green":
-                mainPanel.setColor(Color.GREEN);
-                break;
-            case "blue":
-                mainPanel.setColor(Color.BLUE);
-                break;
-            case "black":
-                mainPanel.setColor(Color.BLACK);
-                break;
-            default:
-                mainPanel.setColor(Color.BLACK);
-                break;
-        }
-    }
-
-
-    private void movePointer(String[] command) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
---
+++ src/myresprog/Procedure.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myresprog;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author dennisschmock
+ */
+public class Procedure {
+    private String procName;
+    private int procStart;
+    private int procEnd;
+    private int callPoint;
+    private HashMap localVars = new HashMap();
+    
+    
+    public Procedure(String name, int procStart, int procEnd){
+        this.procName = name;
+        this.procStart = procStart;
+        this.procEnd = procEnd;
+        System.out.println("Name: " + name + " Start: " + procStart + " End: " + procEnd);
+    }
+    
+    public void changeVar(HashMap m,String key, int value){
+        m.replace(key, value);
+    }
+    
+    public boolean containsParameters(){
+        return !localVars.isEmpty();
+    }
+
+    /**
+     * @return the procName
+     */
+    public String getProcName() {
+        return procName;
+    }
+
+    /**
+     * @param procName the procName to set
+     */
+    public void setProcName(String procName) {
+        this.procName = procName;
+    }
+
+    /**
+     * @return the procStart
+     */
+    public int getProcStart() {
+        return procStart;
+    }
+
+    /**
+     * @param procStart the procStart to set
+     */
+    public void setProcStart(int procStart) {
+        this.procStart = procStart;
+    }
+
+    /**
+     * @return the procEnd
+     */
+    public int getProcEnd() {
+        return procEnd;
+    }
+
+    /**
+     * @param procEnd the procEnd to set
+     */
+    public void setProcEnd(int procEnd) {
+        this.procEnd = procEnd;
+    }
+
+    /**
+     * @return the callPoint
+     */
+    public int getCallPoint() {
+        return callPoint;
+    }
+
+    /**
+     * @param callPoint the callPoint to set
+     */
+    public void setCallPoint(int callPoint) {
+        this.callPoint = callPoint;
+    }
+
+    /**
+     * @return the localVars
+     */
+    public HashMap getLocalVars() {
+        return localVars;
+    }
+
+    /**
+     * @param localVars the localVars to set
+     */
+    public void setLocalVars(HashMap localVars) {
+        this.localVars = localVars;
+    }
+    
+}
